3b1850aada5122cbfc3912d467b48eab7654524e
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-05 14:33:36 +0530

    first commit

---
+++ src/main/java/com/example/studentappnew_backend1/StudentappnewBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.studentappnew_backend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentappnewBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentappnewBackend1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentappnew_backend1/StudentappnewBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentappnew_backend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentappnewBackend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
